c29042fbfda2deb3713f6977c343703f3fbe751e
bylay <byly699@gmail.com>
2025-07-03 03:43:50 -0400

    Componentes del formulario
    Se hizo un estado para almacenar los datos del formulario, tambien un manejo de cambios para los inputs (tengo algunos errores, porque me sale, en rojo), el formulario esta ya con campos completables, como el nombre de la jornada, los arboles que se quieren plantar, tipo de zona(se puede elegir entre Urbana o Rural), la fecha con calendario para elegir y por ultimo el boton registrar. Si se apreta el boton sale un aviso en la consola con los datos ingresados

---
+++ app/FormReforestracion.tsx
@@ -0,0 +1,76 @@
+import { Form, Button } from "react-bootstrap";
+import { useState } from "react";
+
+    export default function FormReforestacion() {
+        const [formData, setFormData] = useState({
+            titulo: '',
+            arboles: 0,
+            tipoActividad: 'Urbana',
+            fecha: ''
+        })
+
+
+        const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+            const {name, value } = e.target;
+            setFormData(prev => ({...prev, [name]: value }))
+        };
+
+        const handleSubmit = (e: React.FormEvent) => {
+            e.preventDefault();
+            console.log('Datos del Formulario:', formData);
+        };
+
+        return (
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="f2">
+                    <Form.Label>Nombre de la Jornada</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="titulo"
+                        value={formData.titulo}
+                        onChange={handleChange}
+                        placeholder="Ej: Reforestación Parque Roco"
+                    />
+                </Form.Group>
+
+                <Form.Group className="f2">
+                    <Form.Label>Arboles a plantar</Form.Label>
+                    <Form.Control
+                        type="number"
+                        name="arboles"
+                        min="1"
+                        value={formData.arboles}
+                        onChange={handleChange}
+                    />
+                </Form.Group>
+
+                <Form.Group className="f2">
+                    <Form.Label>Tipo de Zona</Form.Label>
+                        <Form.Select
+                            name="tipoActividad"
+                            value={formData.tipoActividad}
+                            onChange={handleChange}
+                        >
+                            <option value="Urbana">Zona Urbana</option>
+                            <option value="Rural">Zona Rural</option>
+                        </Form.Select>
+                </Form.Group>
+
+                <Form.Group className="f2">
+                    <Form.Label>Fecha de la Jornada</Form.Label>
+                    <Form.Control
+                        type="date"
+                        name="fecha"
+                        value={formData.fecha}
+                        onChange={handleChange}
+                        placeholder="Ej: Reforestación Parque Roco"
+                    />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Registrar
+                </Button>
+            </Form>
+        );
+    }
+
